beec065168cbf145f042d6ab375068ecd21e372f
unknown <binurim95@gmail.com>
2019-05-04 19:38:45 +0530

    add materialize css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import Navbar from "./components/layout/Navbar";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+     
+          <div className="App">
+            <Navbar />
+            
+          </div>
+          </Router>
         
-      
-      </header>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
