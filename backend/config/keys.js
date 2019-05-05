b8f47e503d58b71d3fdfc680f5c59b600c163bff
unknown <binurim95@gmail.com>
2019-05-05 13:14:56 +0530

    setting up API routes

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,8 @@
 module.exports = {
     mongoURI: "mongodb+srv://Binuri:binuri@cluster0-j75lk.mongodb.net/Ajaanz_Bathik?retryWrites=true" 
   };
+
+  module.exports = {
+    mongoURI: "mongodb+srv://Binuri:binuri@cluster0-j75lk.mongodb.net/Ajaanz_Bathik?retryWrites=true",
+    secretOrKey: "secret"
+  };
